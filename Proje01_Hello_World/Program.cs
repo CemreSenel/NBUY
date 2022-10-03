658bc7d46b00bc872b50c470d06bc8199eb46b13
CemreSenel <114918315+CemreSenel@users.noreply.github.com>
2022-10-03 18:13:23 +0300

    ılk uygulama

---
+++ Proje01_Hello_World/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Proje01_Hello_World
+{
+    internal class Program// Burası programın ana scopeunun baslangıc noktası
+    {
+        static void Main(string[] args)// Burası da ılk calısacak olan METOT!!!!
+        {
+            Console.WriteLine("Merhaba, Dünya!");
+            Console.WriteLine("Benım adım Cemre");
+        }
+    }
+}
