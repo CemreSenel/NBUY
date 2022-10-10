c85edfa26b0c8bb74d68a1de7fdeb55f924eb9a4
CemreSenel <114918315+CemreSenel@users.noreply.github.com>
2022-10-10 15:04:42 +0300

    10-10-2022 araya cikmadan once

---
+++ Week_02/Proje01_Class_Field_Property_Uygulama/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Proje01_Class_Field_Property_Uygulama
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Product classımız olacak. Name, price. description
+            // İstenildigi kadar product bılgısının girilmesını saglayacagız
+            // kac adet product bılgısı gırılecegını kullanıcı belırlesın
+            // product ekleme ısı bıtınce eklenmıs productlar lıstelensın
+
+            //Console.Write("kac adet urun gıreceksınız?: ");
+            //int adet = int.Parse(Console.ReadLine());
+            //Product[] products= new Product[adet];
+            //Product product;
+            //for (int i = 0; i < adet; i++)
+            //{
+            //    product = new Product();
+            //    Console.Write("product name: ");
+            //    product.Name = Console.ReadLine();
+
+            //    Console.Write("price: ");
+            //    product.Price=decimal.Parse(Console.ReadLine());
+
+            //    Console.Write("description: ");
+            //    product.Description=Console.ReadLine();
+            //    products[i] = product;  
+            //}
+            //Console.WriteLine("product name\tprice\tdescriptıon");
+            //foreach (var prd in products)
+            //{
+            //    Console.WriteLine($"{prd.Name}\t{prd.Price}\t{prd.Description}");
+            //}
+
+            #region RastgeleDegerUreterek
+            Product[] products = new Product[5];
+            Product product;
+            string[] nameArray = { "galaxy a50", "hp notebook", "macbok air", "ıphone", "lg"};
+            string[] descriptionArray = { "iyidir", "sasırtıcıdır", "heyecan verıcıdır", "solugunuzu keser", "tırttır" };
+            decimal[] oldPrices = new decimal[5];
+            Random random= new Random();
+            Console.WriteLine("yapılacak zam oranını gırınız: ");
+            decimal oran =decimal.Parse(Console.ReadLine());
+             
+            for (int i = 0; i < 5; i++)
+            {
+                product = new Product()
+                {
+                    Name = nameArray[random.Next(0, 5)],
+                    Description = descriptionArray[random.Next(0, 5)],
+                    Price = random.Next(100, 1001),
+                    
+                };
+                oldPrices[i]=product.Price;
+                product.Price*=(1 + (oran/100));
+                products[i] = product;
+            }
+            Console.WriteLine($"product name".PadRight(30) +"oldPrices".PadLeft(10)+ "price".PadRight(10) + "description");
+            int j = 0;
+            foreach (var prd in products)
+            {
+                Console.WriteLine($"{prd.Name.PadRight(30)}{oldPrices[j].ToString().PadRight(10)}{prd.Price.ToString().PadRight(10)}{prd.Description}");
+                j++;
+            }
+            #endregion
+
+
+
+
+        }
+    }
+}
---
+++ Week_02/Proje02_Methods/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Proje02_Methods
+{
+    internal class Program
+    {
+        //static void Topla(int sayi1,int sayi2)
+        //{
+        //    int toplam = sayi1 + sayi2;
+        //    Console.WriteLine();
+        //    Console.WriteLine($"Toplam: {toplam}");
+        //}
+        //static void Cıkar(int sayi1, int sayi2)
+        //{
+        //    int fark = sayi1 - sayi2;
+        //    Console.WriteLine();
+        //    Console.WriteLine($"Fark: {fark}");
+        //}
+        static int Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;   
+        }
+        static int Cıkar(int sayi1, int sayi2)
+        {
+            int fark = sayi1 - sayi2;
+            return fark;
+        }
+        static string 
+
+        static void Main(string[] args)//METHOD
+        {
+            //Console.Write("Birinci sayı: ");
+            //int s1=int.Parse(Console.ReadLine());
+            //Console.Write("ikinci sayı: ");
+            //int s2 = int.Parse(Console.ReadLine());
+
+            //int toplam=Topla(s1,s2);    
+            //int fark=Cıkar(s1,s2);
+
+            //Console.WriteLine($"Toplam: {toplam}, Fark: {fark}");
+            //Console.WriteLine($"{toplam-fark}");
+
+            //Kendısıne verılen metnın ıcınde aradıgınız karakterın kacıncı sırada oldugunu bulan metodu hazırlayınız.
+
+            // Kendısıne verılen metnın ıcınde aradıgımız karakterın olup olmadıgını soyleyen bir metodu hazırlayınız.
+
+            Console.WriteLine("Bir metin giriniz: ");
+            string metin=(Console.ReadLine());
+        }
+    }
+}
