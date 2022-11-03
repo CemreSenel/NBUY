b4531c72aa42cdd0b983aa1f53a7eaad3995335f
CemreSenel <114918315+CemreSenel@users.noreply.github.com>
2022-11-03 11:30:49 +0300

    02-11

---
+++ Week_05/01-JavaScript/FRONTEND-PROJE/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+let notListesi = [
+    {},
+    {},
+    {},
+];
+
+let ul = document.getElementById('note-list');
+  
+for (const not of notListesi) {
+    let li = ` 
+        <li class="note-list-item">
+            <input type="text" id="txtNameNew">
+            <button type="button"  id="btnCheck"><i class="fa-regular fa-square-check"></i></button>
+            <button type="button"  id="btnDelete"><i class="fa-regular fa-trash-can"></i></button>
+        </li>
+    `;
+    ul.insertAdjacentHTML('beforeend',li);
+}
+
+
+
+
+
+
+
+
+
