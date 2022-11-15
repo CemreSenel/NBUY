e6af005c3ac38c95471b0fb0a423fc903cefad75
semakocaoglu <semakocaoglu7@gmail.com>
2022-11-15 18:08:18 +0300

    15-11-2022

---
+++ Week_07/Proje04_MVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proje04_MVC.Controllers;
+
+public class HomeController : Controller
+{
+   
+
+    public IActionResult Index()
+    {
+        string ad="Cemre";
+        ViewBag.KisiAd=ad;
+        string dil="en";
+        string selamlama;
+        selamlama="";
+        if (dil=="Tr")
+        {
+            selamlama="Hosgeldınız";
+        }else if(dil=="en")
+        {
+            selamlama="Welcome";
+        }else{
+            selamlama="";
+        }
+        ViewBag.Selam=selamlama;
+        return View();
+    }
+
+    
+}
