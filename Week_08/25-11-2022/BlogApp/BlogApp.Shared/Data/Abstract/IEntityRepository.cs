d6e082925029f40ca35f30f81c834e9a7f632316
semakocaoglu <114918315+CemreSenel@users.noreply.github.com>
2022-11-25 18:11:13 +0300

    25-11-2022

---
+++ Week_08/25-11-2022/BlogApp/BlogApp.Shared/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,46 @@
+﻿using BlogApp.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Shared.Data.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate,params Expression<Func<T, object>>[] includeProperties); //Not-1,Not-2
+
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate=null, params Expression<Func<T, object>>[] includeProperties); //Not-3
+
+        Task AddAsync(T entity); 
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<bool> AnyAsync(Expression<Func<T,bool>>predicate);
+        Task<int> CountAsync(Expression<Func<T,bool>>predicate);
+
+
+    }
+}
+
+/*
+ Not-1:Örnegın Id si 12 olan Article'ı getırmek ıstıyoruz
+var article = repository.GetAsync(x=>x.Id==12);
+
+User user =repositoryUser.GetAsync(x=>x.Name == "enginniyazi")
+
+
+Not-2:Örnegın Id si 12 olan Article ı User ve Commenlerıyle bırlıkte getırmek ıstıyoruz
+var article = repositoryArticle.GetAsync(x=>x.Id==12,y=>y.User,y=>y.Comments,z=>z.Role)
+
+Not-3:Ornegın tum article ları listelemek ıstıyoruz
+var articles =repositoryArticle.GetAllAsync();
+
+Örnegin DotNet kategorisindekı tum Article ları listelemek istiyoruz
+var articles = repositoryArticle.GetAllAsync(a=>a.Category.Name=="DotNet",c=>c.Category)
+
+Örneğin Id sı 12 olan Article ın yorumlaarını getirmek ıstıyoruz
+var comments = repositoryComment.GetAllAsync(c=>c.ArticleId==12)
+
+ */
---
+++ Week_08/25-11-2022/BlogApp/BlogApp.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,89 @@
+﻿using BlogApp.Shared.Data.Abstract;
+using BlogApp.Shared.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Quic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Shared.Data.Concrete.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity> : IEntityRepository<TEntity>
+        where TEntity : class,
+        IEntity,
+        new()
+ 
+    {
+        private readonly DbContext _context;
+
+        public EfEntityRepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);   
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().AnyAsync(predicate);
+        }
+
+        public async Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().CountAsync(predicate);
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            await Task.Run(() => { _context.Set<TEntity>().Remove(entity); });
+        }
+        public async Task UpdateAsync(TEntity entity)
+        {
+            await Task.Run(() => { _context.Set<TEntity>().Update(entity); });  
+        }
+
+        public async Task<IList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            //Burada context e erısıp hangi entity de calısacaksak o entıty yı IQuerble tıpınde alıyoruz 
+            IQueryable<TEntity> query = _context.Set<TEntity>(); 
+            if (predicate!=null) //Predicate null degılse bu bır kosul belırtmıs demektır.Dolayısıyla bu durumda o kosulu(predıcate) if ıcınde Where ile query uzerıne eklıyorum.Eger predicate null ıse query üzerıne where ile ilgili bır sey eklemememıze gerek yok
+            {
+                query = query.Where(predicate);   
+            }
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties) 
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            //Artık ToListAsync yapılabılecek query elımızde . Bız de gerıye dondurecegımız query yi TOListAsync ile listeye cevırıyoruz cunku bu metot gerıye bır liste dondurmesı gerekıyor.
+            return await  query.ToListAsync();    
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>().Where(predicate);
+            //query=query.Where(predicate);
+            if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return await query.FirstOrDefaultAsync();
+            //return await query.SingleOrDefaultAsync();  
+
+        }
+
+       
+    }
+}
