53702ad453f58396bd3c546f208162dfb1310e40
semakocaoglu <114918315+CemreSenel@users.noreply.github.com>
2022-12-09 18:11:42 +0300

    09-12-2022

--- Week_10/08-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Controllers/CategoryController.cs
+++ Week_10/08-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -7,7 +7,6 @@ namespace ShoppingApp.Web.Areas.Admin.Controllers
     [Area("Admin")]
     public class CategoryController : Controller
     {
-
         private readonly ICategoryService _categoryService;
 
         public CategoryController(ICategoryService categoryService)
