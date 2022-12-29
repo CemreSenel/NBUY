390afc5e77ab32f8baf93d298a48e090edcc5f7e
semakocaoglu <114918315+CemreSenel@users.noreply.github.com>
2022-12-29 15:02:57 +0300

    29-12-2022

---
+++ OzelDers/OzelDers.Entity/Concrete/Student.cs
@@ -0,0 +1,22 @@
+﻿using OzelDers.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OzelDers.Entity.Concrete
+{
+    public class Student : IEntityBase
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Url { get; set; }
+        public string ImageUrl { get; set; }
+        public List<StudentTeacher> StudentTeachers { get; set; }
+
+    }
+}
---
+++ OzelDers/OzelDers.Entity/Concrete/Teacher.cs
@@ -0,0 +1,30 @@
+﻿using OzelDers.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace OzelDers.Entity.Concrete
+{
+    public class Teacher : IEntityBase
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string About { get; set; }
+        public string Experience { get; set; }
+        public string Location { get; set; }
+        public string Url { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal? PricePerHour { get; set; }
+        public int BranchId { get; set; }
+        public Branch Branch { get; set; }
+        public int UniversityId { get; set; }
+        public University University { get; set; }
+        public List<StudentTeacher> StudentTeachers { get; set; }
+    }
+}
